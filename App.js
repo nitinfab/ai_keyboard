6d31fb06e26970367a210f39275c931f53366559
unknown <nitinfab0@gmail.com>
2021-09-28 20:16:18 +0530

    Final Commit

--- App.js
+++ App.js
@@ -1,19 +1,71 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useRef } from 'react';
+import Keyboard from 'react-simple-keyboard';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+import "react-simple-keyboard/build/css/index.css";
 
 export default function App() {
+  const [input, setInput] = useState("");
+  const [layout, setLayout] = useState("default");
+  const keyboard = useRef();
+
+  const onChange = input => {
+    setInput(input);
+    console.log("Input changed", input);
+  };
+
+  const handleShift = () => {
+    const newLayoutName = layout === "default" ? "shift" : "default";
+    setLayout(newLayoutName);
+  };
+
+  const onKeyPress = button => {
+    console.log("Button pressed", button);
+
+    /**
+     * If you want to handle the shift and caps lock buttons
+     */
+    if (button === "{shift}" || button === "{lock}") handleShift();
+  };
+
+  const onChangeInput = event => {
+    const input = event.target.value;
+    setInput(input);
+    keyboard.current.setInput(input);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+     <TextInput
+        style={styles.input}
+        value={input}
+        placeholder={"Tap on the virtual keyboard to start"}
+        onChange={onChangeInput}
+      />
+			<Keyboard
+        style={styles.keyboard}
+        keyboardRef={r => (keyboard.current = r)}
+        layoutName={layout}
+        onChange={onChange}
+        onKeyPress={onKeyPress}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  input: {
+    width: 1000,
+    height: 100,
+    margin: 100,
+    padding: 20,
+    fontSize: 20,
+  },
+  keyboard: {
+    maxWidth: 850,
+  },
   container: {
-    flex: 1,
+    width: "auto",
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
